0e49f96bed771a4334340e872b7a24f133f78064
Alex <49966773+alexchexes@users.noreply.github.com>
2023-10-08 12:57:01 +0500

    Add command for reset font size to default, binded to Ctrl+0

--- half_step_font_size_adjustment.py
+++ half_step_font_size_adjustment.py
@@ -1,14 +1,7 @@
 import sublime
 import sublime_plugin
 
-
-def changeFontSizeBy(value):
-    current_font_size = sublime.load_settings("Preferences.sublime-settings").get('font_size')
-    new_font_size = current_font_size + value
-    settings = sublime.load_settings("Preferences.sublime-settings")
-    settings.set('font_size', new_font_size)
-    sublime.save_settings("Preferences.sublime-settings")
-    sublime.set_timeout(lambda: sublime.status_message('New font size: ' + str(new_font_size)), 0)
+default_font_size = 11.5
 
 
 class IncreaseFontSizeHalfStepCommand(sublime_plugin.TextCommand):
@@ -19,3 +12,26 @@ class IncreaseFontSizeHalfStepCommand(sublime_plugin.TextCommand):
 class DecreaseFontSizeHalfStepCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         changeFontSizeBy(-0.5)
+
+
+class ResetFontSizeToDefaultCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        saveSettingsFontSize(default_font_size)
+        sublime.status_message('Font size was reset to: ' + str(default_font_size))
+
+
+def changeFontSizeBy(value):
+    current_font_size = getSettingsFontSize()
+    new_font_size = current_font_size + value
+    saveSettingsFontSize(new_font_size)
+    sublime.set_timeout(lambda: sublime.status_message('New font size: ' + str(new_font_size)), 0)
+
+
+def getSettingsFontSize():
+    return sublime.load_settings("Preferences.sublime-settings").get('font_size')
+
+
+def saveSettingsFontSize(new_font_size):
+    settings = sublime.load_settings("Preferences.sublime-settings")
+    settings.set('font_size', new_font_size)
+    sublime.save_settings("Preferences.sublime-settings")
